e68c15eeab037dfcda00e0d6e8b7f0a014f8c749
Mubaraq-Ay <ayanlekemubaraq@gmail.com>
2024-10-27 07:23:21 -0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+         
+      ],
+    theme: {
+        extend: {
+            colors: {
+                "black": "#060606"
+            },
+        },
+    },
+    plugins: [],
+};
