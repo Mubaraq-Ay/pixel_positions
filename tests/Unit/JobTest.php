e408fd2d0aa4bdf77aa112b4d20ecd8f9c95b031
Mubaraq-Ay <ayanlekemubaraq@gmail.com>
2024-10-29 13:56:25 -0700

    modified

--- tests/Unit/JobTest.php
+++ tests/Unit/JobTest.php
@@ -9,4 +9,16 @@ it('belongs to an employer', function () {
     $job = Job::factory()->create([
         'employer_id' => $employer->id,
     ]);
+
+    expect($job->employer->is($employer))->toBeTrue();
+});
+
+
+it ('can have tags', function () {
+
+    $job = Job::factory()->create();
+
+    $job->tag('Frontend');
+
+    expect($job->tags)->toHaveCount(1);
 });
